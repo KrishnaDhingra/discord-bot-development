cd2fd15e50ec5d4df71fe29625a562ff981c3096
Krishna Dhingra <dkrishna1608@gmail.com>
2022-04-16 23:28:41 +0530

    Updated git ignore

---
+++ bot.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log('Our bot is ready to go')
+})
+client.login(process.env.BOT_TOKEN)
