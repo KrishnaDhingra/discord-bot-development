1ffc87b26f00ee619004eaefef9bfac9f99d0a75
Krishna Dhingra <dkrishna1608@gmail.com>
2022-04-17 01:40:00 +0530

    Done with setting up everything on discord inorder to give roles to users

--- bot.js
+++ bot.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, Intents } = require('discord.js')
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
 
 client.on('ready', () => {
   console.log('Our bot is ready to go')
+})
+
+client.on('messageCreate', (msg) => {
+  if (msg.content === '!mod-me') {
+    msg.member.roles.add('964973781686820884')
+  }
 })
 client.login(process.env.BOT_TOKEN)
